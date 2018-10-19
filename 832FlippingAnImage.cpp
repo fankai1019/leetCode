580b14f57af7d9d67295e9931dc0e59dea188b96
kai fan <kai.fan.1019@gmail.com>
2018-10-18 22:29:11 -0400

    add solution without stl

--- 832FlippingAnImage.cpp
+++ 832FlippingAnImage.cpp
@@ -15,6 +15,22 @@ class Solution {
     }
 };
 
+class Solution2 {
+   public:
+    std::vector<std::vector<int>> flipAndInvertImage(
+        std::vector<std::vector<int>>& A) {
+        for (auto& row : A) {
+            for (int i = 0; i < row.size() / 2; ++i) {
+                int t = row[i];
+                row[i] = row[row.size() - i - 1] != 1;
+                row[row.size() - i - 1] = t != 1;
+            }
+        }
+
+        return A;
+    }
+};
+
 int main() {
     std::vector<std::vector<int>> matrix = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
     Solution a;
