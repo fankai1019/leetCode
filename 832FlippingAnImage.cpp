89acb32a922af848f157d2bf3b4f8c8c52c0f984
kai fan <kai.fan.1019@gmail.com>
2018-10-18 21:57:12 -0400

    832 Flipping An Image

---
+++ 832FlippingAnImage.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class Solution {
+   public:
+    std::vector<std::vector<int>> flipAndInvertImage(
+        std::vector<std::vector<int>>& A) {
+        for (auto& row : A) std::reverse(row.begin(), row.end());
+
+        for (auto& row : A)
+            for (auto& i : row) i = 1 - i;
+
+        return A;
+    }
+};
+
+int main() {
+    std::vector<std::vector<int>> matrix = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+    Solution a;
+    auto result = a.flipAndInvertImage(matrix);
+
+    for (const auto& row : matrix) {
+        std::cout << '[';
+        for (auto iter = row.cbegin(); iter != row.cend() - 1; ++iter) {
+            std::cout << *iter << ',';
+        }
+        std::cout << row.back() << ']' << std::endl;
+    }
+}
