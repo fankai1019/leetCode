cf364348bfeab9c134398664e86917f1cccf60ec
kai fan <kai.fan.1019@gmail.com>
2018-10-22 21:50:14 -0400

    169 Majority Element

--- 169MajorityElement.cpp
+++ 169MajorityElement.cpp
@@ -1,27 +1,45 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <iostream>
 #include <map>
+#include <vector>
 
-class Solution{
-public:
-    int majorityElement(std::vector<int>& nums){
+// Boyer-Moore majority vote algorithm;
+// This algorithm is an algorithm for finding the majority of a sequence of
+// elements using linear time and constant space;
+
+class Solution {
+   public:
+    int majorityElement(std::vector<int>& nums) {
+        int count = 0;
+        int result;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (count == 0) {
+                result = nums[i];
+                count++;
+            } else if (result == nums[i])
+                count++;
+            else
+                count--;
+        }
+        return result;
+    }
+};
+
+class Solution2 {
+   public:
+    int majorityElement(std::vector<int>& nums) {
         std::map<int, int> mymap;
         int max = 0;
-        for(int i = 0; i < nums.size(); ++i){
-            mymap[nums[i]]++;
-            max = std::max(max, mymap[i]);
-        }
+        for (int i = 0; i < nums.size(); ++i)
+            max = std::max(max, ++mymap[nums[i]]);
 
-        for(auto& i : mymap)
-            if(i.second == max)
-                return i.first;
+        for (auto& i : mymap)
+            if (i.second == max) return i.first;
         return 0;
     }
 };
 
-int main(){
-
+int main() {
     std::vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
     Solution a;
     std::cout << a.majorityElement(nums);
