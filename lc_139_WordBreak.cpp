73861de8ddd7af1f52528063ff80c1979fc02cc0
fankai1019 <kai.fan.1019@gmail.com>
2021-12-02 18:07:08 -0500

    leetcode 139 word break

--- lc_139_WordBreak.cpp
+++ lc_139_WordBreak.cpp
@@ -6,9 +6,8 @@
 #include <vector>
 using namespace std;
 
-// TC: worse case for reSpace function O(L^2), where n is num of sentence length {"a"}, "aaaaaaaaj"
-// SP: linear
-// TODO: memo + minimizing the number of unrecognized characters
+// TC: need to do research on trie + dp + memo complexity analysis
+// SC: linear
 class Solution
 {
 public:
@@ -24,7 +23,6 @@ public:
 
         int is_word;
         vector<Node *> children;
-        string word;
     };
 
     Solution() : root_(new Node) {}
@@ -40,51 +38,52 @@ public:
             p = p->children[i];
         }
         p->is_word = true;
-        p->word = word;
     }
 
-    bool reSpace(const vector<string> &str_vec, const string &sentence)
+    bool wordBreak(const string &sentence, const vector<string> &str_vec)
     {
         for (const string &str : str_vec)
             insert(str);
         vector<string> words;
-        bool result = helper(sentence, 0, words);
+        // memo is mainly use for memorizing failures
+        vector<int> memo(sentence.size(), -1);
+        bool result = helper(sentence, 0, memo);
         string new_sentence;
         for (size_t i = words.size() - 1; i < words.size(); --i)
             new_sentence += words[i] + ' ';
         new_sentence.pop_back();
-        cout << new_sentence << endl;
         return result;
     }
 
 private:
-    bool helper(const string &sentence, size_t j, vector<string> &words)
+    bool helper(const string &sentence, size_t j, vector<int> &memo)
     {
         if (j == sentence.size())
             return true;
+        if (memo[j] != -1)
+            return memo[j];
         const Node *p = root_.get();
         for (size_t i = j; i < sentence.size(); ++i)
         {
             char c = sentence[i];
             p = p->children[c - 'a'];
             if (!p)
-                return false;
-            if (p->is_word && helper(sentence, i + 1, words))
-            {
-                words.push_back(p->word);
-                return true;
-            }
+                return memo[j] = false;
+            if (p->is_word && helper(sentence, i + 1, memo))
+                return memo[j] = true;
         }
-        return false;
+        return memo[j] = false;
     }
     unique_ptr<Node> root_;
 };
 
 int main()
 {
-    vector<string> str_vec = {"jess", "jes", "looked", "just", "liked", "like", "tim", "he", "her", "brother"};
-    string str = "jesslookedjustliketimherbrother";
+    vector<string> str_vec = {"cats", "dog", "sand", "and", "cat"};
+    string str = "catsandog";
+    // vector<string> str_vec = {"leet", "code"};
+    // string str = "leetcode";
     Solution s;
-    cout << s.reSpace(str_vec, str) << endl;
+    cout << s.wordBreak(str, str_vec) << endl;
     return 0;
 }
