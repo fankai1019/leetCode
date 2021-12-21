66e10b77b0f6b1f300350cd0a11a7e5027a2425f
fankai1019 <kai.fan.1019@gmail.com>
2021-12-20 19:27:36 -0500

    added dp solution

--- lc_55_JumpGame.cpp
+++ lc_55_JumpGame.cpp
@@ -9,9 +9,10 @@
 #include "utils.h"
 using namespace std;
 
+// Greedy
 // TC: O(n) we need to loop over all elements
 // SP: O(1) only previous and the one before previous state need to be stored
-class Solution
+class Solution1
 {
 public:
     bool canJump(vector<int> &nums)
@@ -26,9 +27,48 @@ public:
     }
 };
 
+// DP
+// TC: O(n^2) n states and in each state the worst case needs to loop n elements
+// SP: O(n) memo
+// state: can I jump to end from position i
+class Solution2
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        size_t n = nums.size();
+        vector<int> memo(nums.size(), -1);
+        return helper(nums, 0, n, memo);
+    }
+
+private:
+    // Returns whether we can reach n - 1 from i
+    bool helper(const vector<int>& nums, int i, size_t n, vector<int>& memo)
+    {
+        if(memo[i] != -1)
+            return memo[i];
+        if (i >= n - 1 || i + nums[i] >= n-1)
+        {
+            memo[i] = 1;
+            return true;
+        }
+        for(size_t j=1; j<=nums[i]; ++j)
+        {
+            if (helper(nums, i+j, n, memo))
+            {
+                memo[i] = 1;
+                return true;
+            }
+        }
+        memo[1] = 0;
+        return false;
+    }
+};
+
 int main()
 {
-    Solution s;
+    // Solution1 s;
+    Solution2 s;
     vector<int> ivec = {2, 3, 1, 1, 4};
     bool result = s.canJump(ivec);
     cout << result << endl;
