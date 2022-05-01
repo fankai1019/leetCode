0205beb39f0afa32803c55bd9cb5c1653695e86e
fankai1019 <kai.fan.1019@gmail.com>
2022-05-01 12:46:57 -0400

    redid leetcode 53 maximum sub array

--- lc_53_MaximumSubarray.cpp
+++ lc_53_MaximumSubarray.cpp
@@ -28,8 +28,7 @@ public:
     }
 };
 
-// DP: sum(0, i) = a[i] + sum(0, i-1) < 0 ? 0 : sum(0, i-1), recursive
-// DP: sum(0, i) = max(a[i], a[i] + sum(0, i-1))
+// DP: dp[i] = max(a[i], a[i] + dp[i-1])
 // TC: O(n) loops over all elements
 // SC: O(n) stack size is n
 class Solution2
@@ -57,12 +56,35 @@ private:
     int global_max_;
 };
 
+// iterative DP
+// define dp[i] as the maximum array's sum which has nums[i] at the end
+// dp[i] = dp[i-1] < 0 ? nums[i] : nums[i] + max(dp[i-1], 0);
+// TC: O(n) loops over all elements
+// SC: O(n) needs an additional array for dp
+class Solution3
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            dp[i] = dp[i - 1] < 0 ? nums[i] : nums[i] + max(dp[i - 1], 0);
+            result = max(result, dp[i]);
+        }
+        return result;
+    }
+};
+
 // TODO: follow up divide and conquer
 
 int main()
 {
     // Solution1 s;
-    Solution2 s;
+    // Solution2 s;
+    Solution3 s;
     vector<int> ivec = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int result = s.maxSubArray(ivec);
     cout << result << endl;
