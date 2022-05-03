ed44339ada665ec607abcbf5a25a525e25f5a248
fankai1019 <kai.fan.1019@gmail.com>
2022-05-03 19:38:09 -0400

    leetcode 107 binary tree order traversal II

--- lc_107_BinaryTreeOrderTraversalII.cpp
+++ lc_107_BinaryTreeOrderTraversalII.cpp
@@ -7,19 +7,10 @@
 #include "utils.h"
 using namespace std;
 
-// struct TreeNode
-// {
-//   TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//   int val;
-//   TreeNode *left;
-//   TreeNode *right;
-// };
-
+// Reverse
 // TC: O(n): every node is pushed and poped once, so 2n
 // SC: O(n): in worst case you need to hold all vertices in the queue
-class Solution
+class Solution1
 {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode *root)
@@ -51,11 +42,64 @@ public:
     }
 };
 
+// BFS + DFS
+// TC: O(n): bfs traverse once to calculate max depth then dfs traverse once
+// SC: O(n): queue and stack frame size are n
+class Solution2
+{
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode *root)
+    {
+        if (!root)
+            return {};
+
+        int h = depth(root);
+        vector<vector<int>> result(h, vector<int>());
+        traverse(root, 0, result);
+        return result;
+    }
+
+private:
+    void traverse(TreeNode *root, int depth, vector<vector<int>> &result)
+    {
+        if (!root)
+            return;
+        result[result.size() - depth - 1].push_back(root->val);
+        traverse(root->left, depth + 1, result);
+        traverse(root->right, depth + 1, result);
+    }
+    int depth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+
+        queue<TreeNode *> nq;
+        nq.push(root);
+        int depth = 0;
+        while (nq.size())
+        {
+            int size = nq.size();
+            for (int i = 0; i < size; ++i)
+            {
+                TreeNode *node = nq.front();
+                nq.pop();
+                if (node->left)
+                    nq.push(node->left);
+                if (node->right)
+                    nq.push(node->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
+
 int main()
 {
     vector<int> ivec = {3, 9, 20, -1, -1, 15, 7};
     BFS<TreeNode> bfs(ivec);
-    Solution s;
+    // Solution1 s;
+    Solution2 s;
     vector<vector<int>> result = s.levelOrderBottom(bfs.root());
     for (const auto &ivec : result)
     {
