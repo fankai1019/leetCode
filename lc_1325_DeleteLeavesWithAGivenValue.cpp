7c5070cf84fc84e1d978ffb4d165c2f1cc197ba4
fankai1019 <kai.fan.1019@gmail.com>
2022-05-05 00:29:44 -0400

    leetcode 1325 delete leaves with a given value

---
+++ lc_1325_DeleteLeavesWithAGivenValue.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+#include "utils.h"
+
+using namespace std;
+
+// DFS Bottom up
+// TC: O(n) iterate onces
+// SC: O(height)
+class Solution
+{
+public:
+    TreeNode *removeLeafNodes(TreeNode *root, int target)
+    {
+        TreeNode new_root;
+        new_root.left = root;
+        traverse(root, target, &new_root, 0);
+        return new_root.left;
+    }
+
+private:
+    void traverse(TreeNode *root, int target, TreeNode *parent, int left0right1)
+    {
+        if (!root)
+            return;
+        traverse(root->left, target, root, 0);
+        traverse(root->right, target, root, 1);
+        if (root->val == target && !root->left && !root->right)
+        {
+            if (!left0right1)
+                parent->left = nullptr;
+            else
+                parent->right = nullptr;
+        }
+    }
+};
+
+int main()
+{
+    vector<int> ivec = {1, 2, 3, 2, -1, 2, 4};
+    int target = 2;
+    BFS<TreeNode> bfs(ivec);
+    BFS<TreeNode>::print(bfs.root());
+    Solution s;
+    TreeNode *result = s.removeLeafNodes(bfs.root(), target);
+    BFS<TreeNode>::print(result);
+    return 0;
+}
