cc69ab2f680b0beff5e14cdbbb1396332c65ce25
fankai1019 <kai.fan.1019@gmail.com>
2022-05-05 23:25:55 -0400

    redid leetcode 113 path sum II

--- lc_113_PathSumII.cpp
+++ lc_113_PathSumII.cpp
@@ -8,50 +8,33 @@
 #include "utils.h"
 using namespace std;
 
-// struct TreeNode
-// {
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(long x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(long x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//     long val;
-//     TreeNode *left;
-//     TreeNode *right;
-// };
-
 // DFS
 // TC: O(n): every node is visited 3 times, so 3n
 // SC: O(h): tree height
 class Solution
 {
 public:
-    vector<vector<int>> pathSum(TreeNode *root, int target_sum)
+    vector<vector<int>> pathSum(TreeNode *root, int targetSum)
     {
-        vector<vector<int>> result;
-        vector<int> state;
-        int sum = 0;
-        dfs(result, state, sum, root, target_sum);
-        return result;
+        vector<int> nodes;
+        traverse(root, 0, targetSum, nodes);
+        return paths;
     }
 
 private:
-    void dfs(vector<vector<int>> &result, vector<int> &state, int sum,
-             TreeNode *root, int target_sum)
+    void traverse(TreeNode *root, int val, int targetSum, vector<int> &path)
     {
         if (!root)
             return;
-
-        state.push_back(root->val);
-        sum += root->val;
-
-        if (sum == target_sum && !root->left && !root->right)
-            result.push_back(state);
-        dfs(result, state, sum, root->left, target_sum);
-        if (root->left)
-            state.pop_back();
-        dfs(result, state, sum, root->right, target_sum);
-        if (root->right)
-            state.pop_back();
+        path.push_back(root->val);
+        int sum = val + root->val;
+        if (!root->left && !root->right && sum == targetSum)
+            paths.push_back(path);
+        traverse(root->left, sum, targetSum, path);
+        traverse(root->right, sum, targetSum, path);
+        path.pop_back();
     }
+    vector<vector<int>> paths;
 };
 
 int main()
@@ -59,7 +42,7 @@ int main()
     int target_sum = 22;
     vector<int> ivec = {5, 4, 8, 11, -1, 13, 4, 7, 2, -1, -1, 5, 1};
     BFS<TreeNode> tree(ivec);
-    tree.print();
+    BFS<TreeNode>::print(tree.root());
     Solution s;
     vector<vector<int>> result = s.pathSum(tree.root(), target_sum);
     printVecVec(result);
