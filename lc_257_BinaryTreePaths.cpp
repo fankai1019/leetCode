50d6e05fcbcc1792adad91a3f2ad6513b410765a
fankai1019 <kai.fan.1019@gmail.com>
2022-05-06 01:03:10 -0400

    leetcode 257 binary tree paths

---
+++ lc_257_BinaryTreePaths.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+#include "utils.h"
+
+using namespace std;
+
+// DFS
+// TC: O(n) iterate onces
+// SC: O(height) stack frame size is h
+class Solution
+{
+public:
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+        vector<string> result;
+        if (!root)
+            return result;
+
+        string currStr;
+        traverse(root, currStr, result);
+        return result;
+    }
+
+private:
+    void traverse(TreeNode* root, string currStr, vector<string>& result)
+    {
+        if(!root)
+            return;
+        currStr += to_string(root->val);
+        if(!root->left && !root->right)
+        {
+            result.push_back(currStr);
+            return;
+        }
+        currStr += "->";
+        traverse(root->left, currStr, result);
+        traverse(root->right, currStr, result);
+    }
+};
+
+int main()
+{
+    vector<int> ivec = {1, 2, 3, -1, 5};
+    BFS<TreeNode> bfs(ivec);
+    BFS<TreeNode>::print(bfs.root());
+    Solution s;
+    vector<string> result = s.binaryTreePaths(bfs.root());
+    printVec(result);
+    return 0;
+}
