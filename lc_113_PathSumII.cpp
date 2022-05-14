a25435e812ae771f4cf9963dc092ecc5d47aa8d3
fankai1019 <kai.fan.1019@gmail.com>
2022-05-14 18:50:58 -0400

    redid leetcode 113 path sum II

--- lc_113_PathSumII.cpp
+++ lc_113_PathSumII.cpp
@@ -11,7 +11,7 @@ using namespace std;
 // DFS
 // TC: O(n): every node is visited 3 times, so 3n
 // SC: O(h): tree height
-class Solution
+class Solution1
 {
 public:
     vector<vector<int>> pathSum(TreeNode *root, int targetSum)
@@ -37,13 +37,45 @@ private:
     vector<vector<int>> paths;
 };
 
+// minor diff from solution1 with fewer arguments
+// DFS
+// TC: O(n): every node is visited 3 times, so 3n
+// SC: O(h): tree height
+class Solution2
+{
+public:
+    vector<vector<int>> pathSum(TreeNode *root, int targetSum)
+    {
+        vector<int> path;
+        backtracking(root, path, targetSum);
+        return results;
+    }
+
+    void backtracking(TreeNode *root, vector<int> &path, int targetSum)
+    {
+        if (!root)
+            return;
+
+        path.push_back(root->val);
+
+        if (!root->left && !root->right && root->val == targetSum)
+            results.push_back(path);
+
+        backtracking(root->left, path, targetSum - root->val);
+        backtracking(root->right, path, targetSum - root->val);
+        path.pop_back();
+    }
+    vector<vector<int>> results;
+};
+
 int main()
 {
     int target_sum = 22;
     vector<int> ivec = {5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1};
     BFS<TreeNode> tree(ivec);
     print(tree.root());
-    Solution s;
+    // Solution1 s;
+    Solution2 s;
     vector<vector<int>> result = s.pathSum(tree.root(), target_sum);
     printVecVec(result);
 
