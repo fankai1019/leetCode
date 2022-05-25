8c107a63c56fa78cb4ce5af2487e682d4bb061b8
fankai1019 <kai.fan.1019@gmail.com>
2022-05-25 00:11:53 -0400

    redid leetcode 99 recover binary search tree

--- lc_99_RecoverBinarySearchTree.cpp
+++ lc_99_RecoverBinarySearchTree.cpp
@@ -8,69 +8,90 @@
 #include "utils.h"
 using namespace std;
 
-// struct TreeNode
-// {
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(long x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(long x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//     long val;
-//     TreeNode *left;
-//     TreeNode *right;
-// };
-
-// DFS
-// TC: O(n): all nodes are visited
-// SC: O(h): depth of the tree
+// DFS cleaner code but slower
+// TC: O(nh) binary search
+// SC: O(h) recursion stack frame size is h
 class Solution1
 {
 public:
     void recoverTree(TreeNode *root)
     {
-        if (!root)
-            return;
-        head_.reset(new TreeNode(INT_MIN));
-        prev_ = head_.get();
-        first_ = nullptr;
-        second_ = nullptr;
-        inOrder(root);
-        swap(first_, second_);
+        TreeNode dummy;
+        while (!isBST(root))
+            dfs(root, &dummy);
     }
 
 private:
-    void inOrder(TreeNode *root)
+    void dfs(TreeNode *root, TreeNode *parent, long mn = LONG_MIN, long mx = LONG_MAX)
     {
         if (!root)
             return;
-        inOrder(root->left);
-        if (!first_ && root->val < prev_->val)
-            first_ = prev_;
-        if (first_ && root->val < prev_->val)
-            second_ = root;
-        prev_ = root;
-        inOrder(root->right);
+
+        if (mn >= root->val || mx <= root->val)
+            swap(root->val, parent->val);
+
+        dfs(root->left, root, mn, root->val);
+        dfs(root->right, root, root->val, mx);
     }
 
-    void swap(TreeNode *node1, TreeNode *node2)
+    bool isBST(TreeNode *root, long mn = LONG_MIN, long mx = LONG_MAX)
     {
-        if (node1 && node2)
-        {
-            int tmp = node1->val;
-            node1->val = node2->val;
-            node2->val = tmp;
-        }
+        if (!root)
+            return true;
+        if (mn >= root->val || mx <= root->val)
+            return false;
+        if (!isBST(root->left, mn, root->val))
+            return false;
+        if (!isBST(root->right, root->val, mx))
+            return false;
+        return true;
+    }
+};
+
+// DFS
+// TC: O(n): all nodes are visited
+// SC: O(h): depth of the tree
+class Solution2
+{
+public:
+    void recoverTree(TreeNode *root)
+    {
+        dfs(root);
+        swap(first->val, second->val);
     }
 
-    TreeNode *first_;
-    TreeNode *prev_;
-    TreeNode *second_;
-    unique_ptr<TreeNode> head_;
+private:
+    void dfs(TreeNode *root)
+    {
+        if (!root)
+            return;
+        dfs(root->left);
+        // 1 2 3 4 5
+        // 1 3 2 4 5
+        if (prev && prev->val >= root->val)
+        {
+            if (!first)
+            {
+                first = prev;
+                second = root;
+            }
+            else
+                second = root;
+        }
+        prev = root;
+        dfs(root->right);
+    }
+
+    TreeNode *prev = nullptr;
+    TreeNode *first = nullptr;
+    TreeNode *second = nullptr;
 };
 
 // DFS
 // follow up morris traversal
 // TC: O(n): all nodes are visited
 // SC: O(1): stack and recursion are not used
-class Solution2
+class Solution3
 {
 public:
     void recoverTree(TreeNode *root)
@@ -150,7 +171,8 @@ int main()
     BFS<TreeNode> bfs(ivec);
     print(bfs.root());
     // Solution1 s;
-    Solution2 s;
+    // Solution2 s;
+    Solution3 s;
     s.recoverTree(bfs.root());
     print(bfs.root());
 
