2154149ea67f2f23ff0f94d74cce93db9aaecfb6
fankai1019 <kai.fan.1019@gmail.com>
2022-05-25 21:22:29 -0400

    redid leetcode 108 convert sorted array to binary search tree

--- lc_108_ConvertSortedArrayToBinarySearchTree.cpp
+++ lc_108_ConvertSortedArrayToBinarySearchTree.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 // DFS construction
 // TC: O(n) loop over every node.
-// SC: O(nlogn) stack frame size is tree height, which is nlogn for a balanced tree
-class Solution
+// SC: O(logn) stack frame size is tree height, which is logn for a balanced tree
+class Solution1
 {
 public:
     TreeNode *sortedArrayToBST(vector<int> &nums)
@@ -34,11 +34,36 @@ private:
     }
 };
 
+// Redid solution
+// TC: O(n) loop over every node.
+// SC: O(logn) stack frame size is tree height, which is logn for a balanced tree
+class Solution2
+{
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        return dfs(nums, 0, nums.size() - 1);
+    }
+
+private:
+    TreeNode *dfs(const vector<int> &nums, int start, int end)
+    {
+        if (start > end)
+            return nullptr;
+        int mid = (start + end) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = dfs(nums, start, mid - 1);
+        root->right = dfs(nums, mid + 1, end);
+        return root;
+    }
+};
+
 int main()
 {
     vector<int> ivec{-10, -3, 0, 5, 9};
     printVec(ivec);
-    Solution s;
+    // Solution1 s;
+    Solution2 s;
     TreeNode *result = s.sortedArrayToBST(ivec);
     print(result);
     clear(result);
